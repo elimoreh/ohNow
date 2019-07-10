8967cea6bff4dd06e2ecf4f104277f194aa7b96d
elimoreh <elimoreh@berkeley.edu>
2019-07-10 08:12:15 -0700

    implement basic config for redux persist store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,18 @@
 import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist'
+
 import rootReducer from './reducers';
+import storage from 'redux-persist/lib/storage'
 
-const store = createStore(rootReducer);
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+  
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
+  
+  export const store = createStore(persistedReducer)
+  export const persistor = persistStore(store)
+  
 
-export default store;
+
