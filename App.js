571c57f62b22d6af9ed9ee9aa2996921bc17ca8b
elimoreh <elimoreh@berkeley.edu>
2019-07-10 06:10:19 -0700

    add redux provider and  store

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
-// import store from './src/redux/store';
+import store from './src/redux/store';
 
  export default function App(props) {
     return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </View>
+      </Provider>
     );
 }
 
