4394c297c09576a5ea51a1ee24cae25e26c2b1de
elimoreh <elimoreh@berkeley.edu>
2019-07-16 14:14:14 -0700

    add style login and signup bottons for auth

--- src/screens/Auth/AuthOrigin.js
+++ src/screens/Auth/AuthOrigin.js
@@ -1,10 +1,52 @@
 import React from 'react'
-import { Button, View, Text } from 'react-native'
+import { Button, View, Text, StyleSheet } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function AuthOrigin ({navigation}) {
     const _goToLogin = () => {
         navigation.navigate('Login')
     }
-    return(<Button title='Login Here!'onPress={_goToLogin}></Button>)
+    return(
+    <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={_goToLogin}>
+                <Text style={styles.loginButton}>Login</Text>
+            </TouchableOpacity>
+        </View>
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={_goToLogin}>
+                <Text style={styles.loginButton}>Signup</Text>
+            </TouchableOpacity>
+        </View>
+    </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignContent: 'center', 
+      justifyContent: 'center',
+      backgroundColor: 'black'
+    },
+    buttonContainer:{
+        borderColor: 'black',
+        borderLeftWidth: 50,
+        borderRightWidth: 50,
+        borderBottomWidth: 10,
+        borderTopWidth: 10,
+    },
+    loginButton: {
+        color: 'black',
+        backgroundColor: '#5DBCD2',
+        borderColor: 'white',
+        fontWeight: 'bold',
+        borderRadius: 10,
+        fontSize: 18,
+        overflow: 'hidden',
+        padding: 8,
+        textAlign:'center',
+      },
+});
+  
 
