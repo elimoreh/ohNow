b5dcd07def7cbab1f785b24cd03f04fa1b9efe2f
elimoreh <elimoreh@berkeley.edu>
2019-07-15 18:47:17 -0700

    add redux example and placeholder text

--- src/screens/Profile/ProfileScreen.js
+++ src/screens/Profile/ProfileScreen.js
@@ -9,20 +9,24 @@ import {
   View,
 } from 'react-native';
 import { connect } from 'react-redux';
+import { firstAction } from '../../redux/actions'
 
-const mapStateToProps = state => ({ state: state.initialReducer });
+const mapStateToProps = state => ({ loggedIn: state.authReducer.loggedIn });
 
-function ProfileScreen({state}) {
-  console.warn(state);
+
+function ProfileScreen({loggedIn, firstAction}) {
+  // console.warn(state);
+ 
   return (
     <View style={styles.container}>
       <ScrollView>
+        <Text> Hello!</Text>
       </ScrollView>
     </View>
   );
 }
 
-export default connected = connect(mapStateToProps)(ProfileScreen);
+export default ProfileScreen = connect(mapStateToProps)(ProfileScreen);
 
 ProfileScreen.navigationOptions = {
   header: null,
