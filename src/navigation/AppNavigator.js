ba04d96bc6edb8462a7ca57dd2e810b4b45bb007
elimoreh <elimoreh@berkeley.edu>
2019-07-15 18:43:06 -0700

    create switch navigator with auth load and auth stack

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-
 import MainTabNavigator from './MainTabNavigator';
+import AuthLoad from '../screens/Auth/AuthLoading'
+import AuthStack from '../screens/Auth/AuthStack'
 
 export default createAppContainer(
   createSwitchNavigator({
-    // You could add another route here for authentication.
-    // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    Main: MainTabNavigator,
+    Load: AuthLoad,
+    Auth: AuthStack,
+    App: MainTabNavigator,
   })
 );
